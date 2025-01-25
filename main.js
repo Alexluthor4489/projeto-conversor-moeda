f07ff083e5a45bb4f5df715e2c0b3e130d86fea3
Alexluthor4489 <145698841+Alexluthor4489@users.noreply.github.com>
2025-01-24 21:06:27 -0300

    concluindo-projeto

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-let dolar = 6.11
+let dolar = 0
 let usdInput = document.querySelector('#usd')
 let brlInput = document.querySelector('#brl')
 
+updateDollarRate();
+
+
 usdInput.addEventListener('keyup', () => {
-    usdInput.value = formatCurrency(usdInput.value)//Como funciona? Estou pegando o que o usuário digitou, jogando dentro da função que formata, e depois devolvendo no próprio input que o usuário está digitando, mas já de forma formatada em tempo real.
+    convert('usd-to-brl')
 })
 brlInput.addEventListener('keyup', () => {
-    brlInput.value = formatCurrency(brlInput.value)//Como funciona? Estou pegando o que o usuário digitou, jogando dentro da função que formata, e depois devolvendo no próprio input que o usuário está digitando, mas já de forma formatada em tempo real.
+    convert('brl-to-usd')
 })
 
-usdInput.value = '1000,00'
+usdInput.addEventListener("blur", () => {
+    usdInput.value = formatCurrency(usdInput.value)
+})
+brlInput.addEventListener("blur", () => {
+    brlInput.value = formatCurrency(brlInput.value)
+})
+
+usdInput.value = '0'
 convert('usd-to-brl')
 
 function formatCurrency(value){
@@ -35,9 +45,29 @@ function convert(type) {
     if(type == 'usd-to-brl'){
         let fixedValue = fixValue(usdInput.value)
         let result = fixedValue * dolar
+
         result = result.toFixed(2)
+        brlInput.value = formatCurrency(result)
     }
     if(type == 'brl-to-usd'){
+        let fixedValue = fixValue(brlInput.value)
+        let result = fixedValue / dolar
 
+        result= result.toFixed(2)
+        usdInput.value = formatCurrency(result)
     }
+}
+function updateDollarRate() {
+    const apiUrl = 'https://economia.awesomeapi.com.br/json/last/USD-BRL';
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            dolar = parseFloat(data.USDBRL.bid); // Obtém a taxa de câmbio
+            console.log(`Cotação atualizada do dólar: ${dolar}`);
+        })
+        .catch(error => {
+            console.error('Erro ao buscar a cotação do dólar:', error);
+            alert('Não foi possível atualizar a cotação do dólar.');
+        });
 }
